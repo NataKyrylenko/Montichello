ab671ba8b7a1788fd7d25a34c835bdd4ae42c9bd
natakyrylenko <natalik10107919@gmail.com>
2022-01-30 18:23:34 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  $('.menu__burger').click(function(event) {
+      $('.menu__burger,.menu__item').toggleClass('active');
+  });    
+});
+
+
+
+$(document).ready(function(){
+    $('.slick-slider').slick({
+    infinite:true,  
+    arrows: true,
+    dots:true,
+    autoplay:true,
+    autoplaySpeed: 4000,
+    slidesToShow: 3, 
+    slidesToScroll: 1,
+    prevArrow: '<span class="priv_arrow"><i class="fas fa-angle-left"></i></span>',
+    nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 725,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+})
+});
+
+
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.6690343, lng: -73.8691217 },
+    zoom: 12,
+  });
+}
+
