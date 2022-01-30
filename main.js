4ebc4cfffcc6ccf9920e6564d6f066d5dc152d5b
natakyrylenko <natalik10107919@gmail.com>
2022-01-31 01:26:25 +0200

    fix margin for pixel perfect

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ $(document).ready(function(){
     infinite:true,  
     arrows: true,
     dots:true,
-    autoplay:true,
-    autoplaySpeed: 3000,
+    //autoplay:true,
+    //autoplaySpeed: 3000,
     slidesToShow: 3, 
     slidesToScroll: 1,
     prevArrow: '<span class="priv_arrow"><i class="fas fa-angle-left"></i></span>',
@@ -47,7 +47,7 @@ $(document).ready(function(){
         }
       },
       {
-        breakpoint: 775,
+        breakpoint: 715,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1
